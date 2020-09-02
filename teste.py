33493844497a466f8ffeac9b896ca2ab556909c7
rodrigoneal <rodrigho2006@gmail.com>
2020-09-02 19:42:20 -0300

    Versão 0.1.4

--- teste.py
+++ teste.py
@@ -1,34 +1,17 @@
-from core.core import Requisicao
-from datetime import datetime, date, timedelta
-from core.speed import velocidade, perda
-from sqlalchemy.sql import func
-from app.models.relatorio import Qualidade,Relatorio
-from app import create_app
+import webbrowser
+import threading
+import os
 
-'''import webbrowser
-
-webbrowser.open_new_tab('http://127.0.0.1:5000/')
-# testando
-'''
-a = '01:00:00'.split(':')
-
-date = timedelta(hours=int(a[0]), minutes=int(a[1]),seconds=int(a[2]))
-print(int(date.total_seconds()))
-
-"""app = create_app()
-
-with app.app_context():
-    from datetime import datetime
-
-    string = "00:00:01"
-    # Considering date is in dd/mm/yyyy format
+def executa(programa):
+    exec(open(programa).read())
 
 
-    relatorio = Relatorio.query.with_entities(Relatorio.periodo).all()
-    print(relatorio)
-"""
+principal = threading.Thread(target=executa, args=('main.py',))
 
-
-
-
+if __name__ == '__main__':
+    os.system('python -m venv .venv')
+    os.system('pip install requeriments.txt')
+    principal.start()
+    webbrowser.open_new_tab('http://127.0.0.1:5000/')
+    exec(open('app.py').read())
 
