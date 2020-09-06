4217b9d3bb93ba96c99f5fc283f5cab7f75c626a
rodrigoneal <rodrigho2006@gmail.com>
2020-09-06 19:55:39 -0300

    Versão 0.1.4

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='internet',
+    version='0.1.4',
+    packages=['app', 'app.models', 'core'],
+    url='',
+    license='',
+    author='rodri',
+    author_email='',
+    description=''
+)
--- teste.py
+++ teste.py
@@ -2,6 +2,7 @@ import webbrowser
 import threading
 import os
 
+
 def executa(programa):
     exec(open(programa).read())
 
@@ -10,8 +11,6 @@ principal = threading.Thread(target=executa, args=('main.py',))
 
 if __name__ == '__main__':
     os.system('python -m venv .venv')
-    os.system('pip install requeriments.txt')
     principal.start()
     webbrowser.open_new_tab('http://127.0.0.1:5000/')
     exec(open('app.py').read())
-
